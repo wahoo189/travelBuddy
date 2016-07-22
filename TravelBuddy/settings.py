d8f40dafe367e97920944eeff2a198d6c41fe168
Michael Bogdanski <mbogdanski@codingdojo.com>
2016-07-22 07:55:40 -0700

    bug fix

--- TravelBuddy/settings.py
+++ TravelBuddy/settings.py
@@ -91,6 +91,11 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
+
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # }
 }
 
 # Password validation
